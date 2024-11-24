6274b3a70ec8c4ea82a16e5783dee3196224a628
Ahmad Kazmi <ahmad2112004-d@akgec.ac.in>
2024-11-25 00:24:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let loginform = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () => {
+  loginform.classList.toggle("active");
+  navbar.classList.remove("active");
+};
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  loginform.classList.remove("active");
+};
+
+window.onscroll = () => {
+  loginform.classList.remove("active");
+  navbar.classList.remove("active");
+};
